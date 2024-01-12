adf8d91090cf1d4c469a12cc23a2e8452c8447a6
dongtimes2 <dongtimes2@gmail.com>
2024-01-13 04:40:37 +0900

    feat: loading 페이지 텍스트 수정

--- src/app/order/loading.tsx
+++ src/app/order/loading.tsx
@@ -1,7 +1,8 @@
 const Loading = () => {
   return (
-    <main className="flex justify-center items-center h-full">
-      <p>데이터를 불러오고 있습니다.</p>
+    <main className="flex flex-col justify-center items-center h-[calc(100%-226px)]">
+      <p>목록을</p>
+      <p>불러오고 있습니다.</p>
     </main>
   );
 };
