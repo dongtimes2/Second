a5673b696d4a9c5a3456c56e79f85f3f91cb14e7
dongtimes2 <dongtimes2@gmail.com>
2024-01-13 04:18:10 +0900

    feat: zustand 전역 store 구현

---
+++ src/store/order.ts
@@ -0,0 +1,23 @@
+import { create } from 'zustand';
+
+interface OrderState {
+  totalPrice: number;
+  totalAmount: number;
+  setTotalPrice: (updater: (prevTotalPrice: number) => number) => void;
+  setTotalAmount: (updater: (prevTotalAmount: number) => number) => void;
+}
+
+export const useOrderStore = create<OrderState>((set) => ({
+  totalPrice: 0,
+  totalAmount: 0,
+  setTotalPrice: (updater) => {
+    set((state) => {
+      return { totalPrice: updater(state.totalPrice) };
+    });
+  },
+  setTotalAmount: (updater) => {
+    set((state) => {
+      return { totalAmount: updater(state.totalAmount) };
+    });
+  },
+}));
